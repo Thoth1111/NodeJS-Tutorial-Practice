397ce3319d6aed03ed23043e0cb528235d4af922
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-08-29 14:41:55 +0300

    query url strings using url module

---
+++ demo_querystring.js
@@ -0,0 +1,12 @@
+var http = require('http');
+//include the url module for parsing url strings
+var url = require('url');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
+}).listen(8080);
+
+//running the address 'http://localhost:8080/?year=2023&month=August' will return '2023 August'
