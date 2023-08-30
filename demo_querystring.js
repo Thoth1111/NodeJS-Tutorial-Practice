904326f4ddcaa54f8bf1c7870fafe7c25df207ab
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-08-30 14:01:20 +0300

    Creating a file server that opens files

--- demo_querystring.js
+++ demo_querystring.js
@@ -4,6 +4,7 @@ var url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'content-Type': 'text/html'});
+    //.query returns an object containing the query string parameters
     var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month;
     res.end(txt);
