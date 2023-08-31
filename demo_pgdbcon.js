b2f59aae0254abb3bed9189ce75ae84c1dd494d8
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-08-31 11:33:37 +0300

    Connecting a postgreSQL database

---
+++ demo_pgdbcon.js
@@ -0,0 +1,24 @@
+//Destructured import for pool object in pg module
+const { Pool } = require('pg');
+
+const con = new Pool({
+    host: "localhost",
+    user: "postgres",
+    password: "railstutorial",
+    //for initial connection, the db should be specified
+    database: "postgres"
+});
+
+con.connect(function (err, client, done) {
+    if (err) throw err;
+    console.log("Connected!");
+    //create a database using the client object
+    client.query("CREATE DATABASE nodejs_tutes", function (err, result) {
+        //call the done() method to release the client back to the pool
+        done();
+        if (err) throw err;
+        console.log("Database created!");
+        //end the connection
+        con.end();
+    });
+});
